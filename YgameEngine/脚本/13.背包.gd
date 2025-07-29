d3c8774dd0c231706f30135aea3229a53f32c904
zuiaiyangyang3 <zuiaiyangyang3@qq.com>
2025-07-29 14:24:50 +0800

    可以加密的背包

--- YgameEngine/脚本/13.背包.gd
+++ YgameEngine/脚本/13.背包.gd
@@ -2,19 +2,30 @@ extends Node
 class 角色背包:
 	## 道具ID  数量
 	var 背包=[]
+	var 加密=false
 	func 查询背包()->Array:
 		return self.背包
 		# 添加道具到背包
 	func 添加道具(C_道具ID: String, C_操作数量: float=1):
-		print(self.背包)
+		#print(self.背包)
 		# 查找道具是否已在背包中
 		var index = 获取道具索引(C_道具ID)
 		if index != -1:
 			# 如果存在，增加数量
-			self.背包[index]["数量"] += C_操作数量
+			
+			if self.加密:
+				#加密
+				print()
+				var 操作=引擎.加解密.浮点数解密(self.背包[index]["数量"]) + C_操作数量
+				self.背包[index]["数量"]=引擎.加解密.浮点数加密(操作)
+			else:
+				self.背包[index]["数量"] += C_操作数量
 		else:
-			# 如果不存在，添加新道具
-			self.背包.append({"道具ID": C_道具ID, "数量": C_操作数量})
+			if self.加密:
+				self.背包.append({"道具ID": C_道具ID, "数量": 引擎.加解密.浮点数加密(C_操作数量)})
+			else:
+				# 如果不存在，添加新道具
+				self.背包.append({"道具ID": C_道具ID, "数量": C_操作数量})
 
 	# 获取道具在背包中的索引
 	func 获取道具索引(C_道具ID: String) -> int:
@@ -27,19 +38,29 @@ class 角色背包:
 	# 减少背包中的道具 ### 使用减少具时,请先判断数量
 	func 减少道具(C_道具ID: String, C_操作数量: float=1):
 		var index = 获取道具索引(C_道具ID)
-		self.背包[index]["数量"] -= C_操作数量
-
+		#self.背包[index]["数量"] -= C_操作数量
+		if self.加密:
+			#加密
+			var 操作=引擎.加解密.浮点数解密(self.背包[index]["数量"]) - C_操作数量
+			self.背包[index]["数量"]=引擎.加解密.浮点数加密(操作)
+		else:
+			self.背包[index]["数量"] -= C_操作数量
 	
 	# 获取道具在背包中的数量
 	func 获取道具数量(C_道具ID: String) -> float:
 		var index = 获取道具索引(C_道具ID)
 		if index != -1:
-			return self.背包[index]["数量"]
+			if self.加密:
+				return 引擎.加解密.浮点数解密(self.背包[index]["数量"])
+			else:
+				return self.背包[index]["数量"]
 		return 0
 	
 	
-func 创建背包() -> 角色背包:
-	return 角色背包.new()
+func 创建背包(加密:bool=false) -> 角色背包:
+	var a=角色背包.new()
+	a.加密=加密
+	return a
 	
 	
 	
