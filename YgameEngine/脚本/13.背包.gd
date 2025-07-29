00b0b78dbd318f6d6b7a83513a8b89539fdc71f1
zuiaiyangyang3 <zuiaiyangyang3@qq.com>
2025-07-29 14:00:02 +0800

    自动上传

--- YgameEngine/脚本/13.背包.gd
+++ YgameEngine/脚本/13.背包.gd
@@ -5,8 +5,8 @@ class 角色背包:
 	func 查询背包()->Array:
 		return self.背包
 		# 添加道具到背包
-	func 添加道具(C_道具ID: String, C_操作数量: int=1):
-		
+	func 添加道具(C_道具ID: String, C_操作数量: float=1):
+		print(self.背包)
 		# 查找道具是否已在背包中
 		var index = 获取道具索引(C_道具ID)
 		if index != -1:
@@ -16,19 +16,22 @@ class 角色背包:
 			# 如果不存在，添加新道具
 			self.背包.append({"道具ID": C_道具ID, "数量": C_操作数量})
 
-	# 减少背包中的道具 ### 使用减少具时,请先判断数量
-	func 减少道具(C_道具ID: String, C_操作数量: int=1):
-		var index = 获取道具索引(C_道具ID)
-		self.背包[index]["数量"] -= C_操作数量
-
 	# 获取道具在背包中的索引
 	func 获取道具索引(C_道具ID: String) -> int:
 		for i in self.背包.size():
 			if self.背包[i]["道具ID"] == C_道具ID:
 				return i
 		return -1
+	
+
+	# 减少背包中的道具 ### 使用减少具时,请先判断数量
+	func 减少道具(C_道具ID: String, C_操作数量: float=1):
+		var index = 获取道具索引(C_道具ID)
+		self.背包[index]["数量"] -= C_操作数量
+
+	
 	# 获取道具在背包中的数量
-	func 获取道具数量(C_道具ID: String) -> int:
+	func 获取道具数量(C_道具ID: String) -> float:
 		var index = 获取道具索引(C_道具ID)
 		if index != -1:
 			return self.背包[index]["数量"]
@@ -37,3 +40,41 @@ class 角色背包:
 	
 func 创建背包() -> 角色背包:
 	return 角色背包.new()
+	
+	
+	
+	
+	
+###如果有附加 好麻烦
+	#func 添加道具(C_道具ID: String, C_操作数量: int = 1, 回调: Callable = Callable()):
+		#var 临时道具 = {
+			#"道具ID": C_道具ID,
+			#"数量": C_操作数量,
+			#"附加": {}
+		#}
+		#
+		#if 回调.is_valid():
+			#回调.call(临时道具)
+		#
+		#var 回调设置的附加属性 = 临时道具.附加
+		##print("回调中设置的附加属性：", 回调设置的附加属性)
+		#
+		#var index = 获取道具索引(C_道具ID, 回调设置的附加属性)
+		##print("索引：", index, " 当前背包：", self.背包)  # 打印当前背包状态
+		#if index != -1:
+			#self.背包[index]["数量"] += C_操作数量
+		#else:
+			#背包.append(临时道具)
+			##print("添加新道具后：",self.背包)  # 确认添加成功
+#
+	#func 获取道具索引(C_道具ID: String, 目标扩展: Dictionary = {}) -> int:
+		##print("查找索引，当前背包：", 背包)  # 打印当前背包
+		#for i in self.背包.size():
+			#var 道具 =  self.背包[i]
+			#if 道具["道具ID"] != C_道具ID:
+				#continue
+			#
+			#var 道具扩展 = 道具["附加"]
+			#if 道具扩展 == 目标扩展:
+				#return i
+		#return -1
