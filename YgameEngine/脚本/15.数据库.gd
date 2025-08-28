7300539dc8f5b77e70f572a0e054d8aebe35791f
zuiaiyangyang3 <zuiaiyangyang3@qq.com>
2025-08-28 15:46:42 +0800

    新增背包排序

--- YgameEngine/脚本/15.数据库.gd
+++ YgameEngine/脚本/15.数据库.gd
@@ -1,9 +1,12 @@
 extends Node
 
 class 道具数据库:
-	var 道具库;
+	var 道具库={};
+	
 	func _init() -> void:
-		道具库=引擎.文件.读取文件到变量("res://系统/itemsystem.json")
+		if 引擎.文件.是否存在("res://系统/itemsystem.json"):
+			道具库=引擎.文件.读取文件到变量("res://系统/itemsystem.json")
+		
 	func 获取数据库():
 		return 道具库
 	
@@ -32,7 +35,26 @@ class 道具数据库:
 		if 效果 == null:
 			return {}  # 无效果返回空字典
 		return 效果.duplicate()  # 返回副本避免外部修改
+	
+	# 新增获取道具分类函数
+	func 获取道具分类(道具ID: String) -> String:
+		var 道具属性 = 获取道具属性(道具ID, "类型")
+		return 道具属性 if 道具属性!= null else ""
+	# 新增获取道具子类函数
+	func 获取道具子类(道具ID: String) -> String:
+		var 道具属性 = 获取道具属性(道具ID, "子类型")
+		return 道具属性 if 道具属性!= null else ""
 
+	# 新增获取道具等级函数
+	func 获取道具等级(道具ID: String) -> float:
+		var 道具属性 = 获取道具属性(道具ID, "等级")
+		return 道具属性 if 道具属性!= null else 0.0
+
+	# 新增获取道具品质函数
+	func 获取道具品质(道具ID: String) -> float:
+		var 道具属性 = 获取道具属性(道具ID, "品质")
+		return 道具属性 if 道具属性!= null else 0.0
+		
 func 加载道具库() -> 道具数据库:
 	var 道具库=道具数据库.new()
 	return 道具库
